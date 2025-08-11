3c8eba62bc15403bb82d535228ffbd2c935cbb43
tomdkt <tomdkt@gmail.com>
2025-08-11 18:00:34 -0300

    chore: comment out palindrome checker in mainLinkedList.cpp

--- src/semana3/mainLinkedList.cpp
+++ src/semana3/mainLinkedList.cpp
@@ -3,28 +3,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  bool palindrome = true;
-
-  char character;
-
-  StackLinkedList stack;
-  QueueLinkedList queue;
-  cout << "Adicione uma string." << endl;
-  cin.get(character);
-  while (character != '\n') {
-    stack.push(character);
-    queue.enqueue(character);
-    cin.get(character);
-  }
-  while (palindrome && !queue.isEmpty()) {
-    const char stackChar = stack.pop();
-    if (const char queueChar = queue.dequeue(); stackChar != queueChar)
-      palindrome = false;
-  }
-  if (palindrome)
-    cout << "String é Palindrome" << endl;
-  else
-    cout << "String não é palindrome" << endl;
-  return 0;
-}
+// int main() {
+//   bool palindrome = true;
+//
+//   char character;
+//
+//   StackLinkedList stack;
+//   QueueLinkedList queue;
+//   cout << "Adicione uma string." << endl;
+//   cin.get(character);
+//   while (character != '\n') {
+//     stack.push(character);
+//     queue.enqueue(character);
+//     cin.get(character);
+//   }
+//   while (palindrome && !queue.isEmpty()) {
+//     const char stackChar = stack.pop();
+//     if (const char queueChar = queue.dequeue(); stackChar != queueChar)
+//       palindrome = false;
+//   }
+//   if (palindrome)
+//     cout << "String é Palindrome" << endl;
+//   else
+//     cout << "String não é palindrome" << endl;
+//   return 0;
+// }
